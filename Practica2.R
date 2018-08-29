2d9ca7fa868e4417ebbaa93d0ed254080755dce6
IndiraMG <40215375+IndiraMG@users.noreply.github.com>
2018-08-29 16:58:52 -0500

    Update Practica2.R

--- Practica2.R
+++ Practica2.R
@@ -2,31 +2,32 @@
 library("ape")   #cargar la libreria
 data("bird.orders")   #cargar datos
 plot(bird.orders, no.margin = T)  #graficar arbol
-bs.ml <- c(NA, 88, 76, 73, 71, 100, 45, 81, 72, 67, 63, 100, 100, 88, 76, 73, 71,
-           100, 45, 72, 67, 100, 100) #crear o cargar vector con valores de bootstrap
+#crear o cargar vector con valores de bootstrap
+bs.ml <- c(NA, 88, 76, 73, 71, 100, 45, 81, 72, 67, 63, 100, 100, 88, 76, 73, 71, 100, 45, 72, 67, 100, 100) 
 color<-character(length(bs.ml))  #crear vector de colores con la longitus de bs.ml
-color[bs.ml >= 90] <- "red"   #asignar "rojo" cuando soporte es mayor o igual a 90
-color[bs.ml < 90 & bs.ml >= 70] <- "orange"  #asignar "orange" cuando soporte es menor a 90 y mayor o igual a 70
-color[bs.ml < 70] <- "yellow"  #asignar "yellow" cuando soporte es menor a 70
+color[bs.ml >= 90] <- "red"   #asignar rojo cuando soporte es mayor o igual a 90
+color[bs.ml < 90 & bs.ml >= 70] <- "orange"  #asignar naranja cuando soporte es menor a 90 y mayor o igual a 70
+color[bs.ml < 70] <- "yellow"  #asignar amarillo cuando soporte es menor a 70
 nodelabels(node = 25:45, pch = 21, cex = 2, bg = color[-1])  #agregar etiquetas a los nodos
-legend("topleft", legend = c("90 <= BP", "70 <= BP < 90", "BP < 70"),  #agregar leyenda
-       pch = 21, pt.bg = c("red","orange","yellow"), pt.cex = 2, bty = "n")
+legend("topleft", legend = c("90 <= BP", "70 <= BP < 90", "BP < 70"), pch = 21, pt.bg = c("red","orange","yellow"), 
+       pt.cex = 2, bty = "n") #agregar leyenda en esquina superior izquierda
 
 ### Ejercicio 2 ###
-relaciones <- which.edge(bird.orders, 1:5) #Guardar en vector, las relaciones que involucran los nodos 1 al 5
-color2 <- rep("black", Nedge(bird.orders)) #Crear vector con color negro y tantas posiciones como nodos hay
+relaciones <- which.edge(bird.orders, 1:5) #Guardar en vector las relaciones que involucran los nodos 1 al 5
+color2 <- rep("black", Nedge(bird.orders)) #Crear vector con color negro y longitud igual a nodos totales
 color2[relaciones] <- "blue"  #cambiar las posiciones de color2, que estan guardadas en relaciones, por "blue"
-plot.phylo(bird.orders, "phylogram", T, edge.color = color2, #Dibujar filograma usando color2 para las ramas seleccionadas
-     edge.width = 2, no.margin = F, show.tip.label = T)  
+#Dibujar filograma usando color2 para las ramas seleccionadas:
+plot.phylo(bird.orders, "phylogram", T, edge.color = color2, edge.width = 2, no.margin = F, show.tip.label = T)  
 
-info_plot<-plot(bird.orders) #guardar información del gráfico del arbol
+info_plot<-plot(bird.orders) #guardar información del gráfico del arbol en un vector
 plot(bird.orders,show.tip.label = F, x.lim = info_plot$x.lim ) #Graficar sin nombres
-tiplabels(bird.orders$tip.label, adj = -0.1, # agregar nombres con los colores deseados
-          col = c(rep("blue",5), rep("black", 18)), frame="n")
+# agregar nombres con los colores deseados:
+tiplabels(bird.orders$tip.label, adj = -0.1, col = c(rep("blue",5), rep("black", 18)), frame="n")
 
 
-## Ejercicio 3: Función que recibe árbol y vector con estados de caracter y devuelve
-## filograma con ramas coloreadas dependiendo del tipo de cambio:
+## Ejercicio 3: Función que recibe árbol y vector con estados de caracter y devuelve filograma con ramas coloreadas dependiendo 
+#del tipo de cambio:
+
 col.cambio <- function(vector, arbol) {
   
   if (length(vector)==sum(length(arbol$tip.label), arbol$Nnode)) # verificar que la longitud del vector corresponda a los nodos totales del árbol
@@ -47,7 +48,6 @@ col.cambio <- function(vector, arbol) {
   #mensaje de error en caso de que no se cumpla la primera condición
 }
 
-col.cambio(vector = carac, arbol = bird.orders)
 
 
 
